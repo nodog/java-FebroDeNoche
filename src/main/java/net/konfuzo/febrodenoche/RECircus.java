ebf526dd21e63fc3a6d30289125a19d7d935ddb9
Anderson Mills <nodog@konfuzo.net>
2023-10-23 21:33:26 -0500

    Make RECircus realer

--- src/main/java/net/konfuzo/febrodenoche/RECircus.java
+++ src/main/java/net/konfuzo/febrodenoche/RECircus.java
@@ -12,17 +12,39 @@ public class RECircus implements RenderEngine {
 
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setPaint(Color.magenta);
+        g2.setPaint(Color.black);
         g2.fillRect(0, 0, width, height);
-        g2.setPaint(Color.red);
-        int cx = width/2;
-        int cy = height/2;
-        int dia = width*3/4;
-        Ellipse2D e = new Ellipse2D.Double(cx - dia/2, cy - dia/2, dia, dia);
-        g2.fill(e);
-        g2.draw(e);
-        g2.setColor(Color.black);
-        g2.fillOval(0, 0, width/3, height/4);
+
+        for (float fi = 0.0f; fi <= 1.01f; fi += 1.0f/100f) {
+            float bright = (1.0f - fi)/2.0f;
+            g2.setPaint(Color.getHSBColor(0.4f, 1.0f, bright));
+            int cx = (int)(fi*width);
+            int cy = 0;
+            int dia = (int)(fi*width/2.0);
+            Ellipse2D e = new Ellipse2D.Double(cx - dia/2, cy - dia/2, dia, dia);
+            g2.fill(e);
+            g2.draw(e);
+            g2.setPaint(Color.getHSBColor(0.24f, 1.0f, bright));
+            cx = width;
+            cy = (int)(fi*width);
+            e = new Ellipse2D.Double(cx - dia/2, cy - dia/2, dia, dia);
+            g2.fill(e);
+            g2.draw(e);
+            g2.setPaint(Color.getHSBColor(0.2f, 1.0f, bright));
+            cx = (int)((1.0 - fi)*width);
+            cy = height;
+            e = new Ellipse2D.Double(cx - dia/2, cy - dia/2, dia, dia);
+            g2.fill(e);
+            g2.draw(e);
+            g2.setPaint(Color.getHSBColor(0.17f, 1.0f, bright));
+            cx = 0;
+            cy = (int)((1.0-fi)*width);
+            e = new Ellipse2D.Double(cx - dia/2, cy - dia/2, dia, dia);
+            g2.fill(e);
+            g2.draw(e);
+            //g2.setColor(Color.black);
+            //g2.fillOval(0, 0, width/3, height/4);
+        }
         g2.dispose();
         return image;
     }
